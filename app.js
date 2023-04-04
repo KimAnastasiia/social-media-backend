0b292d623b8ceb23fd2475ac64e911e4752765dd
Anastasiia <anstasiakim2789@gmail.com>
2023-04-04 12:06:42 +0200

    added endpoint to create a post

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 4000
 app.use(express.static('public'));
-const routerUsers =  require("./routerUsers")
 app.use(express.json())
-const objectOfApiKey = require("./objectApiKey")
 var cors = require('cors')
 app.use(cors())
 
+
+const objectOfApiKey = require("./objectApiKey")
+const routerUsers =  require("./routerUsers")
+const routerMediaPost = require('./routerMediaPost');
 // IMPORTANT for UPLOAD pictures
 var fileUpload = require('express-fileupload');
 app.use(fileUpload());
@@ -16,9 +18,26 @@ app.use(fileUpload());
 app.use(express.json())
 const jwt = require("jsonwebtoken");
 
+app.use(["/mediaPost"] ,(req,res,next)=>{
+    let apiKey = req.query.apiKey
+  
+    let obj = objectOfApiKey.find((obj)=>
+      obj===apiKey
+    )
+    if(!obj){
+        res.send({error:"error"})
+        return
+    }
+    
+    let infoInToken = jwt.verify(apiKey, "secret");
+    req.infoInToken = infoInToken;
+
+    next()
+})
+
 
 app.use("/users", routerUsers)
-
+app.use("/mediaPost", routerMediaPost)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- routerMediaPost.js
+++ routerMediaPost.js
@@ -2,6 +2,73 @@ const express = require('express');
 const mysqlConnection = require("./mysqlConnection")
 const crypto = require('crypto');
 const routerMediaPost = express.Router();
-let keyEncrypt = 'password';
-let algorithm = 'aes256'
 
+routerMediaPost.get("/",(req,res,next)=>{
+
+    let emailUser = req.query.email
+
+    mysqlConnection.query("SELECT name FROM user where email ='"+emailUser+"' ", (err, rows) => {
+
+        if (err){
+            res.send({error:err});
+            return 
+        } else {
+            console.log(rows);
+        }
+
+        res.send(rows)
+    })
+})
+
+routerMediaPost.get("/post",(req,res,next)=>{
+
+    mysqlConnection.query("SELECT * FROM post where userId =" + req.infoInToken.userId, (err, rows) => {
+
+        if (err){
+            res.send({error:err});
+            return 
+        } else {
+            console.log(rows);
+        }
+
+        res.send(rows)
+    })
+})
+
+
+routerMediaPost.post('/', (req, res) => {
+
+    let img = req.files.myImage
+   
+    mysqlConnection.query("INSERT INTO post (userId) VALUES  ( "+ req.infoInToken.userId+" ) ", (errPost , rowsPost) => {
+
+        if (errPost){
+            res.send({error: errPost});
+            return ;
+        }
+        console.log(rowsPost)
+
+        mysqlConnection.query("SELECT * FROM user WHERE id="+ req.infoInToken.userId, (errorUser, rowsUser) => {
+            if (errorUser){
+                res.send({error: errorUser});
+                return ;
+            }
+            if(rowsUser.length>0){
+                if (img != null) {
+
+                    img.mv('public/images/' + req.infoInToken.userId.toString() +req.infoInToken.email + rowsPost.insertId.toString()  +'.png', 
+                        function(err) {
+                            if (err) {
+                                res.send("Error in upload picture");
+                            } 
+                        }
+                    )
+                }
+    
+            }
+        })
+    })
+
+})
+
+module.exports=routerMediaPost

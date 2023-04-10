9ad32f0cc3f48190f4f2e711432dd326bebeb8f7
Anastasiia <anstasiakim2789@gmail.com>
2023-04-10 20:49:48 +0200

    added endpoint /:id for delete comment in routerComments

--- routerComments.js
+++ routerComments.js
@@ -26,4 +26,29 @@ routerComments.get("/:postId",(req,res,next)=>{
     })
   
 })
+routerComments.delete("/:id",(req,res)=>{
+    let id = req.params.id
+    let apiKey = req.query.apiKey
+  
+    let obj = objectOfApiKey.find((obj)=>
+      obj===apiKey
+    )
+    if(!obj){
+        res.send({error:"error"})
+        return
+    }
+    
+    let infoInToken = jwt.verify(apiKey, "secret");
+    req.infoInToken = infoInToken;
+    mysqlConnection.query("DELETE FROM comment WHERE postId="+id+" and userId="+req.infoInToken.userId,(err,rows)=>{
+        if (err){
+            res.send({error: err});
+            return ;
+        }
+        else{
+            console.log(rows)
+        }
+        res.send({messege:"done"})
+    }
+)})
 module.exports=routerComments

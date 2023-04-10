7dcb7a56d3561b7287b3ad8097b3abf9dc4ad034
Anastasiia <anstasiakim2789@gmail.com>
2023-04-10 20:13:11 +0200

    added information about the owner of the comment

--- routerComments.js
+++ routerComments.js
@@ -10,7 +10,12 @@ routerComments.get("/:postId",(req,res,next)=>{
  
     let postId=req.params.postId
 
-    mysqlConnection.query("SELECT * FROM comment where postId="+postId, (err, rows) => {
+    mysqlConnection.query(`SELECT comment.userId, comment.comment, comment.postId, 
+        comment.date, comment.id, user.name, user.surname, user.uniqueName
+        FROM comment 
+        JOIN user 
+        ON comment.userId=user.id 
+        where comment.postId=`+postId, (err, rows) => {
         if (err){
             res.send({error:err});
             return 

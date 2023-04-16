4587273bcc1a3804fd27b4d7fbddc52d6fd5910a
Anastasiia <anstasiakim2789@gmail.com>
2023-04-16 15:30:08 +0200

    added endpoint for counting likes on a post

--- routerPublicPostLike.js
+++ routerPublicPostLike.js
@@ -6,6 +6,22 @@ const sharp = require('sharp');
 const objectOfApiKey = require("./objectApiKey")
 const jwt = require("jsonwebtoken");
 
+routerPublicPostLike.get("/:postId",(req,res,next)=>{
 
+    let postId = req.params.postId
+
+    mysqlConnection.query(`SELECT COUNT(id) as totalLikes FROM likesofpost
+    WHERE postId =` + postId, (err, rows) => {
+
+        if (err){
+            res.send({error:err});
+            return 
+        } else {
+            console.log(rows);
+        }
+
+        res.send(rows)
+    })
+})
 
 module.exports=routerPublicPostLike

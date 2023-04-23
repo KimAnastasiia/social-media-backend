7969e732f2975ccd4fb004f1849dd8a44a3e4607
Anastasiia <anstasiakim2789@gmail.com>
2023-04-23 21:14:03 +0200

    add endpoint to check subscription

--- routerFriends.js
+++ routerFriends.js
@@ -8,6 +8,22 @@ const objectOfApiKey = require("./objectApiKey")
 const jwt = require("jsonwebtoken");
 const sharp = require('sharp');
 
+routerFriends.get("/",(req,res,next)=>{
 
+    let friendId = req.query.friendId
+    mysqlConnection.query("SELECT * from friends WHERE userId="+req.infoInToken.userId+" and friendId="+friendId, (err, rows) => {
+
+        if (err){
+            res.send({error:err});
+            return 
+        } else if(rows.length>0){
+            res.send({message:true});
+            return 
+        }else if(rows.length==0){
+            res.send({message:false});
+            return 
+        }
+    })
+})
 module.exports=routerFriends
  

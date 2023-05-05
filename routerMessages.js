1f6c4898bf222b3d47226fb5a375dae447cd39ed
Anastasiia <anstasiakim2789@gmail.com>
2023-05-05 22:15:44 +0200

    add endpoint post /messages to send messages

--- routerMessages.js
+++ routerMessages.js
@@ -8,7 +8,23 @@ const objectOfApiKey = require("./objectApiKey")
 const jwt = require("jsonwebtoken");
 const database= require("./database")
 
+routerMessages.post("/",async(req,res)=>{
 
+    let idReceiver = req.body.idReceiver
+    let message= req.body.message
+    const d = Date.now();
+
+    database.connect();
+    try{
+        const sentMessage = await database.query("INSERT INTO messages (date, idSender, idReceiver, message) VALUES  ( "+ d+", "+ req.infoInToken.userId+",  "+ idReceiver+", '"+message+"' ) ")
+        database.disConnect();
+        res.send({message:"done"})
+        return 
+    } catch (error){
+        console.log("Error")
+        res.send({message:"error sending message"})
+    }
+})
 
 
 module.exports=routerMessages
